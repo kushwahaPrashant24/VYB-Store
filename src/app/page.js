cf3084d6788a199e4dde34a69f9dae93df3b55b2
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-11-19 16:24:00 +0530

    completed the website

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,8 @@
-import Navbar from "./pages/navbar";
-import Hero from "./pages/Hero";
 import Login from "./pages/Login" 
 
 export default function Home() {
   return (
     <div className="text-white flex flex-col gap-[36px] w-full">
- {/*
- <Navbar />
-  <Hero />
-  */} 
   <Login />
 </div>
   );
